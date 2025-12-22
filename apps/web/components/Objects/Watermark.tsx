5d0ae813e0e01b2e2e310094998a99cd003a1c0f
Erico Vasconcelos <ericovasconcelos@gmail.com>
2025-12-21 21:07:02 -0300

    feat(ui): remove 'Made with LearnHouse' watermark

--- apps/web/components/Objects/Watermark.tsx
+++ apps/web/components/Objects/Watermark.tsx
@@ -11,16 +11,6 @@ function Watermark() {
     }
         , [org]);
 
-    if (org?.config?.config?.general?.watermark) {
-        return (
-            <div className='fixed bottom-8 right-8'>
-                <Link href={`https://www.learnhouse.app/?source=in-app`} className="flex items-center cursor-pointer bg-white/80 backdrop-blur-lg text-gray-700 rounded-2xl p-2 light-shadow text-xs px-5 font-semibold space-x-2">
-                    <p>Made with</p>
-                    <Image unoptimized src={blacklogo} alt="logo" quality={100} width={85} />
-                </Link>
-            </div>
-        )
-    }
     return null
 }
 
